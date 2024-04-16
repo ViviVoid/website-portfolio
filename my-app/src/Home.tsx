38c98be127e6a4cfd68f7690b1a7272456093afb
Andy Dao <andydao2020@gmail.com>
2024-04-16 00:42:57 -0500

    temporary additions - Lg

--- my-app/src/Home.tsx
+++ my-app/src/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from "./logo.svg";
 import SideNavigationBar from "./SideNavigationBar";
+import Schedule from "./Schedule";
 
 const Home:React.FC = () => (
         <div>
-            <SideNavigationBar />
+            {/*<SideNavigationBar />*/}
+            <Schedule />
         </div>
         // <App />
     );
